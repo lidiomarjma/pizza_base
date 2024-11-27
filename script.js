4be6a0e5f0a4c963f21de7daa3e02bc1e2d84200
lidiomar jma <lidiomarmillaalves@gmail.com>
2024-11-27 06:21:49 -0300

    Adicionando funcionalidade no botão do carrinho.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let modalKey = 0;
+let cart = [];
 let modalQt = 1;
 
 const c = (el)=>document.querySelector(el);
@@ -16,6 +18,7 @@ pizzaJson.map((item, index)=>{
         e.preventDefault();
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
         modalQt = 1;
+        modalKey = key;
 
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -71,4 +74,15 @@ cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
         c('.pizzaInfo--size.selected').classList.remove('selected');
         size.classList.add('selected');
     });
+});
+
+c('.pizzaInfo--addButton').addEventListener('click', ()=>{
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+
+    cart.push({
+        id:pizzaJson[modalKey].id,
+        size,
+        qt:modalQt
+    });
+    closeModal();
 });
